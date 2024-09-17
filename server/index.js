db460a22b19b84ca0b7208500d8090a8f7fdb44f
Alkesh shukla <shukla2002alkesh@gmail.com>
2024-09-17 23:32:26 +0530

    Add next function

--- server/index.js
+++ server/index.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
 });
 
 // app.use(cors({ origin: true }));
